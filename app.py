948f6060f703a2be9fb2c03e47e420eb6c24fc60
Varghese36 <varghesepoulose19@gmail.com>
2025-07-14 21:49:39 +0530

    Bind Flask app to 0.0.0.0 and Render PORT

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
+
 
 app = Flask(__name__)
 
@@ -21,4 +23,5 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
